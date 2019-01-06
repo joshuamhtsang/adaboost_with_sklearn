8a3cd5aa1b4b7b87e5a9f21ab59a416ea0be1d4a
joshuamhtsang <joshua.mh.tsang@gmail.com>
2019-01-06 19:14:58 +0000

    Expanding adaboost example a bit a print out 2 classifiers

--- adaboost_decisiontree.py
+++ adaboost_decisiontree.py
@@ -1,16 +1,36 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 import graphviz
 
 # The training examples.
 
 X = [[1, 1], [0, 0], [1, 0], [0, 1]]
-Y = [1, 0, 1, 1]
+Y = [1, -1, 1, 1]
 
 # Create and fit an AdaBoosted decision tree
 abc = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
                          n_estimators=200)
-
 abc.fit(X, Y)
 
+print(abc.estimator_weights_)
+
+# Output graph for estimator 0
+
+dot_data = tree.export_graphviz(abc.estimators_[0], out_file=None,
+                                class_names=['-1', '+1'])
+graph = graphviz.Source(dot_data)
+graph.render("abc1")
+
+# Output graph for estimator 1
+
+dot_data = tree.export_graphviz(abc.estimators_[1], out_file=None,
+                                class_names=['-1', '+1'])
+graph = graphviz.Source(dot_data)
+graph.render("abc2")
+
 print(abc.predict([[0., 0.]]))
+print(abc.predict([[1., 1.]]))
+print(abc.predict([[1., 0.]]))
+print(abc.predict([[0., 1.]]))
+
