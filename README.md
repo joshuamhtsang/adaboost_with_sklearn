b8715cf10238db313712ba50f713ee64779ef621
joshuamhtsang <joshua.mh.tsang@gmail.com>
2019-01-06 17:08:42 +0000

    Add note about installing virtualenv

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ Testing functionality of sklearn library on different datasets.
 
 # Creating Python 3 virtual environment.
 
+Ensure you have virtualenv installed:
+
+$ pip3 install virtualenv
+
+Now initialize virtualenv: 
+
 $ virtualenv -p python3 .
 
--- decisiontree.py
+++ decisiontree.py
@@ -1,5 +1,14 @@
 from sklearn import tree
-X = [[0, 0], [1, 1]]
-Y = [0, 1]
+X = [[0, 0], [0, 1], [1, 1]]
+Y = [1, 0, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
+
+print(clf.predict([[0., 0.]]))
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
