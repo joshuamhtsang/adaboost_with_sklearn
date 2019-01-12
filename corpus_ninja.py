96ffbcd905907fb40371e308d206d5a24727298f
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-01-12 10:49:12 +0000

    function to read json files and load into python dictionaries.

---
+++ corpus_ninja.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+def read_json_file(filename):
+    objs = []
+
+    with open(filename, 'r') as file_handle:
+        for line in file_handle:
+            print(line)
+            objs.append(json.loads(line))
+
+    return objs
+
+
+if __name__ == '__main__':
+    tracks_list = read_json_file('small.json')
+
+    print(tracks_list[0]["track title"])
