83e9314d4e2c2d5e44b9372a176d6b56d6c18be6
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-01-12 14:43:42 +0000

    Making vocab set

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # adaboost_with_sklearn
 Testing functionality of sklearn library on different datasets.
 
+
 # Creating Python 3 virtual environment.
 
 Ensure you have virtualenv installed:
@@ -10,6 +11,7 @@ $ pip3 install virtualenv
 Now initialize virtualenv: 
 
 $ virtualenv -p python3 .
+
 
 #  Install graphviz on system and corresponding Python module.
 
@@ -24,3 +26,9 @@ $ brew install graphviz
 Then you also need to install the python package:
 
 $ pip3 install graphviz
+
+
+# corpus_ninja.py usage:
+
+python3 corpus_ninja.py --json_corpus small.json
+
--- corpus_ninja.py
+++ corpus_ninja.py
@@ -1,5 +1,6 @@
 import os
 import json
+import argparse
 
 
 def read_json_file(filename):
@@ -17,6 +18,19 @@ def read_json_file(filename):
 
 
 if __name__ == '__main__':
-    tracks_list = read_json_file('small.json')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json_corpus", help="JSON file with desired corpus.",
+                        required=True)
+    args = parser.parse_args()
 
+    tracks_list = read_json_file(args.json_corpus)
     print(tracks_list[0]["track title"])
+
+    # Make a X, Y dataset based on track title.
+    vocab_track_title = set()
+    for i, obj in enumerate(tracks_list):
+        print(i, obj)
+        for word in obj["track title"].split(" "):
+            vocab_track_title.add(word)
+
+    print(vocab_track_title)
