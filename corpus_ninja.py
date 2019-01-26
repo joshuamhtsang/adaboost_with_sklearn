b69614a512d3c4566a4eb8aa0cc7c3a4dd68e4cf
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-01-26 23:37:35 +0000

    Added word frequency anaylsis with Collections Counter

--- corpus_ninja.py
+++ corpus_ninja.py
@@ -1,8 +1,10 @@
 import os
 import json
 import argparse
+from collections import Counter
 
 
+# Reads a JSON file and returns a list of dicts.
 def read_json_file(filename):
     objs = []
 
@@ -18,19 +20,24 @@ def read_json_file(filename):
 
 
 if __name__ == '__main__':
+    # Parse command line args.
     parser = argparse.ArgumentParser()
     parser.add_argument("--json_corpus", help="JSON file with desired corpus.",
                         required=True)
     args = parser.parse_args()
 
+    # Load the JSON file contents as a list of dicts.
     tracks_list = read_json_file(args.json_corpus)
     print(tracks_list[0]["track title"])
 
-    # Make a X, Y dataset based on track title.
-    vocab_track_title = set()
+    # Perform word frequency analysis of track title.
+    vocab_list = []
     for i, obj in enumerate(tracks_list):
-        print(i, obj)
         for word in obj["track title"].split(" "):
-            vocab_track_title.add(word)
+            vocab_list.append(word)
+    print(vocab_list)
+    word_freq = Counter(vocab_list)
+    print(word_freq)
 
-    print(vocab_track_title)
+    # Make a X, Y dataset based on track title.
+
